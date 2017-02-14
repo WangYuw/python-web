faad23be99c4990a7cef184dfecbcf1ec9173952
wang_yuw <yuwei.wang.pluie@gmail.com>
2017-02-14 13:35:11 +0100

    modify orm.py

--- www/orm.py
+++ www/orm.py
@@ -1,76 +1,84 @@
 import asyncio
 import aiomysql
+import logging
 
-@asyncio.coroutine
-#创建一个全局变量__pool，当需要连接数据库的时候可以直接从__pool中获取连接
-def create_pool(loop, **kw):
+# 创建一个全局变量__pool，当需要连接数据库的时候可以直接从__pool中获取连接
+async def create_pool(loop, **kw):
     logging.info('create database connection pool...')
     global __pool
-    __pool=yield from aiomysql.create_pool(
-        host=kw.get('host','localhost'),
-        port=kw.get('port',3306),
+    __pool = await aiomysql.create_pool(
+        host=kw.get('host', 'localhost'),
+        port=kw.get('port', 3306),
         user=kw['user'],
         password=kw['password'],
         db=kw['db'],
-        charset=kw.get('charset','utf8'),
-        autocommit=kw.get('autocommit',True),
-        #最大连接数10 ，最小连接数1
-        maxsize=kw.get('maxsize',10),
-        minsize=kw.get('minsize',1),
+        charset=kw.get('charset', 'utf8'),
+        autocommit=kw.get('autocommit', True),
+        # 最大连接数10 ，最小连接数1
+        maxsize=kw.get('maxsize', 10),
+        minsize=kw.get('minsize', 1),
         loop=loop
     )
 
-@asyncio.coroutine
-#将执行sql的代码封装进select函数，调用的时候只要传入sql，和sql需要的一些参数值
-def seclect(sql, args, size=None):
-    log(sql,args)
+# 输出信息，知道程序在做什么
+def log(sql, args=()):
+    logging.info('SQL: %s' % sql)
+
+# 将执行sql的代码封装进select函数，调用的时候只要传入sql，和sql需要的一些参数值
+async def select(sql, args, size=None):
+    log(sql, args)
     global __pool
-    #从连接池中获取一个数据库连接
-    with(yield from __pool)as conn:
-        cur=yield from conn.cursor(aiomysql.DictCursor)
-        #把sql中的字符串占位符？换成python的占位符%s
-        yield from cur.execute(sql.replqce('?', '%s'), args or ())
-        if size:
-            rs = yield from cur.fetchmany(size)
-        else:
-            rs = yield from cur.fetchall()
-        yield from cur.close()
+    # 从连接池中获取一个数据库连接
+    async with __pool.get() as conn:
+        async with conn.cursor(aiomysql.DictCursor) as cur:
+            # 把sql中的字符串占位符？换成python的占位符%s
+            await cur.execute(sql.replace('?', '%s'), args or ())
+            if size:
+                rs = await cur.fetchmany(size)
+            else:
+                rs = await cur.fetchall()
         logging.info('rows returned: %s' % len(rs))
         return rs
 
-@asyncio.coroutine
-def execute(sql, args): 
-    #INSERT、UPDATE、DELETE
+async def execute(sql, args):
+    # INSERT、UPDATE、DELETE
     log(sql)
-    with (yield from __pool) as conn:
+    async with __pool.get() as conn:
+        if not autocommit:
+            await conn.begin()
         try:
-            cur = yield from conn.cursor()
-            yield from cur.execute(sql.replace('?', '%s'), args)
-            affected = cur.rowcount
-            yield from cur.close()
+            async with conn.cursor(aiomysql.DictCursor) as cur:
+                await cur.execute(sql.replace('?', '%s'), args)
+                affected = cur.rowcount
+            if not autocommit:
+                await conn.commit()
         except BaseException as e:
+            if not autocommit:
+                await conn.rollback()
             raise
         return affected
 
-#在元类中被引用，作用是创建一定数量的占位符
+
+# 在元类中被引用，作用是创建一定数量的占位符
 def create_args_string(num):
     L = []
     for n in range(num):
         L.append('?')
     return ', '.join(L)
 
-#首先、元类找出例如User类在数据库中对应的表名，对User类的自有属性逐条进行分析，找出主键和非主键，同时把这些属性全部存入mappings这个dict
-#然后、删除User类的全部属性，因为实际操作数据库的时候用不到这些属性
-#最后、把操作数据库需要用到的属性添加进去，这包括所有字段和字段类型的对应关系，类对应的表名、主键名、非主键名，还有四句sql语句
-#这些属性才是操作数据库正真需要用到的属性，但仅仅只有这些属性还是不够，因为没有方法
-#而Model类就提供了操作数据库要用到的方法
+
+# 首先、元类找出例如User类在数据库中对应的表名，对User类的自有属性逐条进行分析，找出主键和非主键，同时把这些属性全部存入mappings这个dict
+# 然后、删除User类的全部属性，因为实际操作数据库的时候用不到这些属性
+# 最后、把操作数据库需要用到的属性添加进去，这包括所有字段和字段类型的对应关系，类对应的表名、主键名、非主键名，还有四句sql语句
+# 这些属性才是操作数据库正真需要用到的属性，但仅仅只有这些属性还是不够，因为没有方法
+# 而Model类就提供了操作数据库要用到的方法
 
 class ModelMetaclass(type):
     def __new__(cls, name, bases, attrs):
         # 写在最前面防止Model类操作元类,排除了当前类是Model这种情况
-        if name=='Model':
+        if name == 'Model':
             return type.__new__(cls, name, bases, attrs)
-        #当前类只可能是User类、Blog类、Comment类
+        # 当前类只可能是User类、Blog类、Comment类
         # 获取table名称,数据库中对应的表名,如果User类中没有定义__table__属性，那默认表名就是类名
         tableName = attrs.get('__table__', None) or name
         logging.info('found model: %s (table: %s)' % (name, tableName))
@@ -94,19 +102,20 @@ class ModelMetaclass(type):
         for k in mappings.keys():
             attrs.pop(k)
         escaped_fields = list(map(lambda f: '`%s`' % f, fields))
-        attrs['__mappings__'] = mappings # 保存属性和列的映射关系
+        attrs['__mappings__'] = mappings  # 保存属性和列的映射关系
         attrs['__table__'] = tableName
-        attrs['__primary_key__'] = primaryKey # 主键属性名
-        attrs['__fields__'] = fields # 除主键外的属性名
+        attrs['__primary_key__'] = primaryKey  # 主键属性名
+        attrs['__fields__'] = fields  # 除主键外的属性名
         # 构造默认的SELECT, INSERT, UPDATE和DELETE语句:
         attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ', '.join(escaped_fields), tableName)
-        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (tableName, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
-        attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
+        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (
+            tableName, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
+        attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (
+            tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
         attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
         return type.__new__(cls, name, bases, attrs)
 
 class Model(dict, metaclass=ModelMetaclass):
-
     def __init__(self, **kw):
         super(Model, self).__init__(**kw)
 
@@ -132,21 +141,20 @@ class Model(dict, metaclass=ModelMetaclass):
                 setattr(self, key, value)
         return value
 
-    #这个装饰器是类方法的意思，这样就可以不创建实例直接调用类的方法
+    # 这个装饰器是类方法的意思，这样就可以不创建实例直接调用类的方法
     @classmethod
-    @asyncio.coroutine
-    def find(cls, pk):
-        ' find object by primary key. '
-        rs = yield from select('%s where `%s`=?' % (cls.__select__, cls.__primary_key__), [pk], 1)
+    async def find(cls, pk):
+        # ' find object by primary key. '
+        rs = await select('%s where `%s`=?' % (cls.__select__, cls.__primary_key__), [pk], 1)
         if len(rs) == 0:
             return None
         return cls(**rs[0])
 
     @classmethod
-    #根据WHERE条件查找
+    # 根据WHERE条件查找
     async def findAll(cls, where=None, args=None, **kw):
         ' find objects by where clause. '
-        sql=[cls.__select__]
+        sql = [cls.__select__]
         if where:
             sql.append('where')
             sql.append(where)
@@ -170,9 +178,8 @@ class Model(dict, metaclass=ModelMetaclass):
         rs = await select(' '.join(sql), args)
         return [cls(**r) for r in rs]
 
-
     @classmethod
-    #根据WHERE,SELECT条件查找，返回数字
+    # 根据WHERE,SELECT条件查找，返回数字
     async def findNumber(cls, selectField, where=None, args=None):
         ' find number by select and where. '
         sql = ['select %s _num_ from `%s`' % (selectField, cls.__table__)]
@@ -186,7 +193,6 @@ class Model(dict, metaclass=ModelMetaclass):
 
     ## save、update、remove这三个方法需要管理员权限才能操作，所以不定义为类方法，需要创建实例之后才能调用
 
-    @asyncio.coroutine
     def save(self):
         args = list(map(self.getValueOrDefault, self.__fields__))
         args.append(self.getValueOrDefault(self.__primary_key__))
@@ -194,15 +200,13 @@ class Model(dict, metaclass=ModelMetaclass):
         if rows != 1:
             logging.warn('failed to insert record: affected rows: %s' % rows)
 
-    @asyncio.coroutine
     async def update(self):
         args = list(map(self.getValue, self.__fields__))
         args.append(self.getValue(self.__primary_key__))
         rows = await execute(self.__update__, args)
         if rows != 1:
-            logging.warn('failed to update by primary key: affected rows: %s' % rows)
+            logging.warning('failed to update by primary key: affected rows: %s' % rows)
 
-    @asyncio.coroutine
     async def remove(self):
         args = [self.getValue(self.__primary_key__)]
         rows = await execute(self.__delete__, args)
@@ -211,7 +215,6 @@ class Model(dict, metaclass=ModelMetaclass):
 
 
 class Field(object):
-
     def __init__(self, name, column_type, primary_key, default):
         self.name = name
         self.column_type = column_type
@@ -221,29 +224,27 @@ class Field(object):
     def __str__(self):
         return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
 
-class StringField(Field):
 
+class StringField(Field):
     def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
         super().__init__(name, ddl, primary_key, default)
 
-class BooleanField(Field):
 
+class BooleanField(Field):
     def __init__(self, name=None, default=False):
         super().__init__(name, 'boolean', False, default)
 
-class IntegerField(Field):
 
+class IntegerField(Field):
     def __init__(self, name=None, primary_key=False, default=0):
         super().__init__(name, 'bigint', primary_key, default)
 
-class FloatField(Field):
 
+class FloatField(Field):
     def __init__(self, name=None, primary_key=False, default=0.0):
         super().__init__(name, 'real', primary_key, default)
 
-class TextField(Field):
 
+class TextField(Field):
     def __init__(self, name=None, default=None):
         super().__init__(name, 'text', False, default)
-
-
