1ae90286e2cecd253fb64f2e5643c71c8c745670
wang_yuw <yuwei.wang.pluie@gmail.com>
2017-02-15 14:34:25 +0100

    some comments for models.py

--- www/models.py
+++ www/models.py
@@ -1,7 +1,14 @@
 import time, uuid
+# uuid  生成唯一id的库
+
+# 在编写ORM时，给一个Field增加一个default参数可以让ORM自己填入缺省值，非常方便。并且，缺省值可以作为函数对象传入，在调用save()时自动计算。
+# 例如，主键id的缺省值是函数next_id，创建时间created_at的缺省值是函数time.time，可以自动设置当前日期和时间。
 
 from orm import Model, StringField, BooleanField, FloatField, TextField
 
+# 基于时间的唯一的id，来作为数据库表中每一行的主键
+# time.time() 返回当前时间的时间戳(相对于1970.1.1 00:00:00以秒计算的偏移量)
+# uuid4()——由伪随机数得到，有一定的重复概率，该概率可以计算出来。
 def next_id():
     return '%015d%s000' % (int(time.time() * 1000), uuid.uuid4().hex)
 
